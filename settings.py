739cb4d80b17f0044b3c967da8fd4b4ffb4053cb
Tyler Jones <josephtylerjones@gmail.com>
2016-12-16 17:01:47 -0500

    added settings.py

---
+++ settings.py
@@ -0,0 +1,7 @@
+# Paste your consumer key and consumer secret here
+consumer_key = ''
+consumer_secret = ''
+
+# Paste access token and access token secret here
+access_token = ''
+access_token_secret = ''
